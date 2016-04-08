97947377e110598c2a93f6c7992eb4207f76f1ed
YuanJin <1186296377@qq.com>
2016-04-08 21:44:14 +0800

    添加了程序图标

--- Ziti2/src/com/example/ziti2/MainActivity.java
+++ Ziti2/src/com/example/ziti2/MainActivity.java
@@ -1,34 +1,32 @@
 package com.example.ziti2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	private Button btn_Go;
+	private Xianshi xianshi; 
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		btn_Go= (Button)findViewById(R.id.btn_ture);
+		xianshi= (Xianshi)findViewById(R.id.XianshiView);
+		btn_Go.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO 自动生成的方法存根
+				System.out.println("xianshi.Go();");
+				xianshi.Go();
+				System.out.println("xianshi.Go();");
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-}
+}	
